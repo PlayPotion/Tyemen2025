8eeff274642383e4ac77c6b3052422bfd6cba2c1
ashybadtranslater <ashybadtranslater@gmail.com>
2025-05-05 15:02:56 +0500

    Movement Workaet

--- ElephantScript/Assets/GameFolder/Scripts/MovementThirdPerson.cs
+++ ElephantScript/Assets/GameFolder/Scripts/MovementThirdPerson.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MovementThirdPerson : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float rotateSpeed = 4f;
+    Rigidbody rb;
+
+
     void Start()
     {
-        
+        if (rb == null) rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        if (moveInput.magnitude >= 0.1f) {
+            Quaternion _rotation = Quaternion.LookRotation(moveInput);
+            _rotation.x = 0;
+            _rotation.z = 0;
+            transform.rotation = Quaternion.Lerp(transform.rotation, _rotation, rotateSpeed * Time.deltaTime);
+            
+        }
+
+        rb.velocity = moveInput * speed;
     }
 }
