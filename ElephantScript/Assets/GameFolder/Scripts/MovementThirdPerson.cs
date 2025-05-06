84edd264f371ee0e449c3c2720b02d2222127fb6
ashybadtranslater <ashybadtranslater@gmail.com>
2025-05-07 03:20:50 +0500

    Commit ONO HODIIIIT

--- ElephantScript/Assets/GameFolder/Scripts/MovementThirdPerson.cs
+++ ElephantScript/Assets/GameFolder/Scripts/MovementThirdPerson.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MovementThirdPerson : MonoBehaviour
@@ -8,18 +9,20 @@ public class MovementThirdPerson : MonoBehaviour
     public float speed = 4f;
     [SerializeField] private float rotateSpeed = 4f;
     Rigidbody rb;
+    Animator anim;
 
 
     void Start()
     {
-        if (rb == null) rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
 
         if (moveInput.magnitude >= 0.1f) {
             Quaternion _rotation = Quaternion.LookRotation(moveInput);
@@ -28,7 +31,7 @@ public class MovementThirdPerson : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, _rotation, rotateSpeed * Time.deltaTime);
             
         }
-
+        anim.SetBool("isWalk", moveInput.magnitude >= 0.1f);
         rb.velocity = moveInput * speed;
     }
 }
--- ElephantScript/Assets/GameFolder/Scripts/ZoneChangeCamera.cs
+++ ElephantScript/Assets/GameFolder/Scripts/ZoneChangeCamera.cs
@@ -6,12 +6,20 @@ public class ZoneChangeCamera : MonoBehaviour
 {
 
     public GameObject _setActive;
-    public GameObject _setDeactive;
+    public GameObject other1;
+    public GameObject other2;
+    public GameObject other3;
+    public GameObject other4;
+    public GameObject other5;
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
             _setActive.SetActive(true);
-            _setDeactive.SetActive(false);
+            other1.SetActive(false);
+            other2.SetActive(false);
+            other3.SetActive(false);
+            other4.SetActive(false);
+            other5.SetActive(false);
         }
     }
 
